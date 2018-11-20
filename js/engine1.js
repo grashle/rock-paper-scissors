e5deb79368beb13271d3027a104750f106c2a7af
gracydesouza <gracyde@gmail.com>
2018-11-20 11:59:06 +0000

    index1 and engine1.js work

--- js/engine1.js
+++ js/engine1.js
@@ -1,22 +1,32 @@
 /*variables we need for the game*/
 
 var choices = ["rock", "paper", "scissors"] /*we are assigning values to a list here, remember the first item in a list is assigned 0. all lists start at 0. */
+var you_played;
+var game_won = 0;
+var game_lost = 0;
+var game_drawn = 0;
+var machine_played;
+var game_played = 0;
 
-var played = 0;
-var won = 0;
-var lost = 0;
-var draw = 0;
 
 /*listen for the button clicks*/
 
-$("#rock").click(function(event) {
+$('button').click(function(event){
+  var you_played = this.getAttribute("value");
+
+  $("#play_you").text(you_played);
+  computer_move();
+  compare(you_played, machine_played);
+
+});
+
+/* $("#rock").click(function(event) {
   var you_played ='rock';
   $("#play_you").text(you_played);
 
   var play_machine = computer_move();
   compare(you_played, play_machine);
 });
-
 
 $("#paper").click(function(event) {
   var you_played='paper';
@@ -32,23 +42,17 @@ $("#scissors").click(function(event) {
 
   var play_machine = computer_move();
   compare(you_played, play_machine);
-});
-
+});  */
 
 function computer_move() { /*note the bracket format*/
-  var machine_played = Math.floor(Math.random() * (3 - 0)) + 0;  /*Math.floor guarantees an integer instead of a decimal. the 3 signifies the topmost number plus one*/
-
-  machine_played = choices[machine_played];
+  var random_number = Math.floor(Math.random() * (3 - 0)) + 0;  /*Math.floor guarantees an integer instead of a decimal. the 3 signifies the topmost number plus one*/
+  machine_played = choices[random_number];
   $("#machine_played").text(machine_played);
   console.log('move: ' + machine_played);
-
-  $("game_won").empty();
-  $("game_lost").empty();
-  $("game_drawn").empty();
-
-  return machine_played;
+//  $("game_won").empty();
+//  $("game_lost").empty();
+//  $("game_drawn").empty();
 }
-
 
 function compare(you, machine){
 
@@ -70,23 +74,34 @@ function compare(you, machine){
     verdict = 'win';
   }
 
-  if ( verdict == 'win') {
-    won++;
-    $("#game_won").text(won);
+  if ((you == "paper") && (machine == "scissors")) {
+    verdict = 'lose';
+  }
+  if ((you == "rock") && (machine == "paper")) {
+    verdict = 'lose';
+  }
+  if ((you == "rock") && (machine == "scissors")) {
+    verdict = 'win';
   }
 
-  if ( verdict == 'lose') {
-    lost++;
-    $("#game_lost").text(lost);
-  }
-  if ( verdict == 'draw') {
-    draw++;
-    $("#game_drawn").text(draw);
-  }
+  switch (verdict) {
+      case 'win':
+      game_won++;
+      $("#game_won").text(game_won);
+      break;
+    case 'lose':
+      game_lost++;
+      $('#game_lost').text(game_lost);
+      break;
+    case 'draw':
+      game_drawn++;
+      $("#game_drawn").text(game_drawn);
+      break;
+    }
 
   $("#verdict").text(verdict);
 
-  played++;
-  $("#game_played").text(played);
+  game_played++;
+  $("#game_played").text(game_played);
 
 }
